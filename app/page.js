70a7d49fb8ef40c96c81b686bf564c65e12b431e
htshabitats-gg <hts.habitats@gmail.com>
2026-04-18 21:27:15 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -380,34 +380,12 @@ export default function HomePage() {
           </div>
 
           <div className="lodgify-widget-wrap">
-            <div
-              id="lodgify-search-bar"
-              data-website-id="538798"
-              data-language-code="fr"
-              data-checkout-page-url="https://checkout.lodgify.com/occitanieetlocation/fr/#/634566"
-              data-dates-check-in-label="Arrivée"
-              data-dates-check-out-label="Départ"
-              data-guests-counter-label="Invités"
-              data-guests-input-singular-label="{{NumberOfGuests}} invité"
-              data-guests-input-plural-label="{{NumberOfGuests}} invités"
-              data-location-input-label="Emplacement"
-              data-search-button-label="Rechercher"
-              data-dates-input-min-stay-tooltip-text='{"one":"Minimum {minStay} nuit","other":"Minimum de {minStay} nuits"}'
-              data-guests-breakdown-label="Invités"
-              data-adults-label='{"one":"adulte","other":"adultes"}'
-              data-adults-description="Âges {minAge} ou plus"
-              data-children-label='{"one":"enfant","other":"enfants"}'
-              data-children-description="Âges {minAge} - {maxAge}"
-              data-children-not-allowed-label="Non adapté aux enfants"
-              data-infants-label='{"one":"bébé","other":"bébés"}'
-              data-infants-description="Moins de {maxAge}"
-              data-infants-not-allowed-label="Non adapté aux bébés"
-              data-pets-label='{"one":"animal de compagnie","other":"animaux de compagnie"}'
-              data-pets-not-allowed-label="Non autorisé"
-              data-done-label="Terminé"
-              data-new-tab="true"
-              data-version="stable"
-              data-has-guests-breakdown
+            <iframe
+              src="https://checkout.lodgify.com/occitanieetlocation/fr/#/634566"
+              width="100%"
+              height="600"
+              style={{ border: "none", borderRadius: "12px" }}
+              title="Réservation Cocon Bohème"
             />
           </div>
 
